304ce64edf6eacc48ce7be52090143ddadd1fa45
xhoanabrace <85332819+xhoanabrace@users.noreply.github.com>
2022-06-07 23:26:46 +0200

    Update Destinationen.java

--- Destinationen.java
+++ Destinationen.java
@@ -1,5 +1,62 @@
 package Flugbuchung;
 
-public class Destinationen {
 
-}
+public class Destinationen {
+	
+	 
+	
+	private String abflug;
+	private String ankunft;
+	private double abflugdatum1;
+	private double abflugdatum2;
+
+
+
+	
+	public Destinationen(String abflug, String ankunft, double abflugdatum1 ,double abflugdatum2) {
+		super();
+		this.abflug = abflug;
+		this.ankunft = ankunft;
+		this.abflugdatum1 = abflugdatum1;
+		
+		this.abflugdatum2 = abflugdatum2;
+	
+
+	}
+
+	public double getAbflugdatum1() {
+		return abflugdatum1;
+	}
+
+
+
+	public double getAbflugdatum2() {
+		return abflugdatum2;
+	}
+
+	
+
+	public String getAbflug() {
+		return abflug;
+	}
+
+	public String getAnkunft() {
+		return ankunft;
+	}
+
+	
+	@Override
+	public String toString() {
+		return abflug + " - " + ankunft + " "+ abflugdatum1+" "+abflugdatum2;
+	}
+
+	public String info() {
+		return abflug;
+	}
+	
+			
+	
+		
+		
+	}
+
