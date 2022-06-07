3c2ea7c9c26759f61be9c473dd4035b454f6aae7
xhoanabrace <85332819+xhoanabrace@users.noreply.github.com>
2022-06-07 23:28:27 +0200

    Update Flugzeug.java

--- Flugzeug.java
+++ Flugzeug.java
@@ -2,8 +2,27 @@ package Flugbuchung;
 
 public class Flugzeug {
 
-	public static void main(String[] args) {
+	private static int sitzplatz = 0;
 
+	private String name;
+
+	
+	public Flugzeug(String name) {
+		super();
+		this.name = name;
+		
+	}
+	
+	public void ticketKaufen() {
+		System.out.println();
+	}
+
+	public static int getSitzplatz() {
+		return sitzplatz;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
