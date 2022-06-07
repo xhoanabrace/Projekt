b6a492a5258528cc18e6b3218cd80d53abdb10fa
xhoanabrace <85332819+xhoanabrace@users.noreply.github.com>
2022-06-07 23:26:10 +0200

    Update Anschrift.java

--- Anschrift.java
+++ Anschrift.java
@@ -1,62 +1,33 @@
-package Flugbuchung;
+package Passagier;
 
+public class Anschrift {
 
-public class Destinationen {
-	
-	 
-	
-	private String abflug;
-	private String ankunft;
-	private double abflugdatum1;
-	private double abflugdatum2;
+	private String strasse;
+	private int hausnummer;
+	private int plz;
+	private String ort;
 
-
-
-	
-	public Destinationen(String abflug, String ankunft, double abflugdatum1 ,double abflugdatum2) {
-		super();
-		this.abflug = abflug;
-		this.ankunft = ankunft;
-		this.abflugdatum1 = abflugdatum1;
-		
-		this.abflugdatum2 = abflugdatum2;
-	
-
+	public Anschrift(String strasse, int hausnummer, int plz, String ort) {
+		this.strasse = strasse;
+		this.hausnummer = hausnummer;
+		this.plz = plz;
+		this.ort = ort;
 	}
 
-	public double getAbflugdatum1() {
-		return abflugdatum1;
+	public String getStrasse() {
+		return strasse;
 	}
 
-
-
-	public double getAbflugdatum2() {
-		return abflugdatum2;
+	public int getHausnummer() {
+		return hausnummer;
 	}
 
-	
-
-	public String getAbflug() {
-		return abflug;
+	public int getPlz() {
+		return plz;
 	}
 
-	public String getAnkunft() {
-		return ankunft;
+	public String getOrt() {
+		return ort;
 	}
 
-	
-	@Override
-	public String toString() {
-		return abflug + " - " + ankunft + " "+ abflugdatum1+" "+abflugdatum2;
-	}
-
-	public String info() {
-		return abflug;
-	}
-	
-			
-	
-		
-		
-	}
-
+}
